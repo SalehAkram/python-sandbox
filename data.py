a020fad77cb4bb2c6509b5201b78f81f56f2f7cf
Saleh Akram <STAkram@hotmail.co.uk>
2023-09-27 16:59:55 +0100

    features added

--- data.py
+++ data.py
@@ -1,4 +1,33 @@
-my_dic = {
-    "saleh": 46,
-    "maryam": 15
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
 }
+
+resources = {
+    "water": 0,
+    "milk": 0,
+    "coffee": 0,
+    "money": 0
+}
+
--- main.py
+++ main.py
@@ -1,3 +1,63 @@
-from data import my_dic
+from data import MENU, resources
 
-print(my_dic)
+
+class ResourceCheckResult:
+    def __init__(self, resources_available, message):
+        self.resources_available = resources_available
+        self.message = message
+
+
+def check_resources(required_ingredients):
+    result = ResourceCheckResult(True, "")
+    for key in required_ingredients:
+        if resources[key] < required_ingredients[key]:
+            result.resources_available = False
+            result.message = result.message + (f"Not enough {key}: required amount {required_ingredients[key]} and "
+                                               f"available amount {resources[key]}: ")
+    return result
+
+
+def insert_coins():
+    coins = {
+        "quarters": {"value": 0.25, "quantity": 0},
+        "dimes": {"value": 0.10, "quantity": 0},
+        "nickles": {"value": 0.05, "quantity": 0},
+        "pennies": {"value": 0.01, "quantity": 0},
+    }
+    for key in coins:
+        quantity = int(input(f"how many {key} "))
+        coins[key]["quantity"] = quantity
+    return coins
+
+
+def calculate_total(coins):
+    total = 0.0
+    for key in coins:
+        total += coins[key]["value"] * coins[key]["quantity"]
+    return total
+
+
+def complete_transaction(required_ingredients, cost):
+    for key in required_ingredients:
+        resources[key] -= required_ingredients[key]
+    resources["money"] += cost
+
+
+machine_on = True
+while machine_on:
+    choice = input("What would you like? (espresso/latte/cappuccino)")
+    ingredients = MENU[choice]["ingredients"]
+    result = check_resources(ingredients)
+    if result.resources_available:
+        coins = insert_coins()
+        total_inserted = calculate_total(coins)
+        if total_inserted < MENU[choice]["cost"]:
+            print(f"message sorry not enough money, refunded {total_inserted} ")
+        else:
+            change = total_inserted - MENU[choice]["cost"]
+            complete_transaction(ingredients,MENU[choice]["cost"])
+            print(f"Enjoy your {choice}")
+            if change > 0:
+                print(f"Here is your change {change}")
+
+
